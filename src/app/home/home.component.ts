72001d3fd5924a4507fa29d640e4c541700aa41a
Tijl Declerck <tijl.declerck7@gmail.com>
2019-06-16 20:16:39 -0500

    updates 16/6

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'home',
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  innerWidth: number;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.innerWidth = window.innerWidth;
+  }
+
+
   constructor() { }
 
   ngOnInit() {
+   this.innerWidth = window.innerWidth;
+  }
+
+  getMarginTop(num) {
+    if (this.innerWidth < 350) {
+      if (num === 1) {
+        return -10;
+      } else {
+        return 0;
+      }
+    } else if (this.innerWidth < 580) {
+      if (num === 1 || num === 2) {
+        return -10;
+      } else {
+        return 0;
+      }
+    } else {
+      return -10;
+    }
   }
 
 }
