c13dffd14df296a55d3de1397b337746b5ed4fe9
Tijl Declerck <tijl.declerck@outlook.com>
2019-06-20 11:46:08 -0500

    update 20/6

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
-import {FormBuilder, Validators} from '@angular/forms';
+import {FormBuilder, ValidationErrors, Validators} from '@angular/forms';
 
 @Component({
   selector: 'navbar',
@@ -12,6 +12,10 @@ export class NavbarComponent implements OnInit {
   modalRef;
 
   contactForm;
+
+  get f() {
+    return this.contactForm;
+  }
 
   constructor(private modalService: NgbModal, private fb: FormBuilder) { }
 
@@ -27,14 +31,28 @@ export class NavbarComponent implements OnInit {
     this.contactForm = this.fb.group({
       'firstName': ['', Validators.required],
       'lastName': ['', Validators.required],
-      'email': ['', Validators.required, Validators.email],
+      'email': ['', [Validators.required, Validators.email]],
       'company': [''],
       'message': ['', Validators.required]
     });
   }
 
   submitForm() {
-    console.log('submitting the form');
+    console.log(' valid or not', this.contactForm.valid);
+    if (this.contactForm.valid) {
+      console.log('submitting the form');
+    } else {
+      console.log('unable to send form');
+      this.getFormValidationErrors();
+      console.log('textarea value', this.f.get('message'));
+    }
+  }
+
+  getFormValidationErrors() {
+    Object.keys(this.contactForm.controls).forEach(key => {
+
+      console.log('keyzzzz', this.f.get(key));
+    });
   }
 
 }
