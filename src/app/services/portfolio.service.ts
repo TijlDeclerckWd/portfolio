c42c48e36c55f9dbd5c7cba7b8ce99c679601297
Tijl Declerck <tijl.declerck@outlook.com>
2019-06-28 18:58:52 -0500

    new adjustments

--- src/app/services/portfolio.service.ts
+++ src/app/services/portfolio.service.ts
@@ -11,7 +11,9 @@ export class PortfolioService {
   openModal = new Subject();
   BASE_API_URL = environment.BASE_API_URL;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    console.log('is production?', environment.production);
+  }
 
   downloadResume() {
     return this.http.get(`${this.BASE_API_URL}/file/downloadFile/CV-English.pdf`, {responseType: 'blob' });
