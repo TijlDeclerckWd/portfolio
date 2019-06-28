843e72ca52d5e73a79e6a0e82e830bb06e8d9e37
Tijl Declerck <tijl.declerck@outlook.com>
2019-06-28 11:51:24 -0500

    added loader spinners

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
+  projectImagePreload = "/assets/images/smartfit-home.png";
+  bgImagePreload = "/assets/images/desk-3.png";
+
+  bgImageLoaded: string;
+  projectImageLoaded: string;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  determineBgImg() {
+    return `linear-gradient(to right, rgba(#fff, .8), rgba(#ddd, .8)),  url(${this.bgImageLoaded})`;
   }
 
 }
