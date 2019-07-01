d1462ddb5b2c08d7aea7616b6691f22626fa7ef8
Tijl Declerck <tijl.declerck@outlook.com>
2019-07-01 18:10:17 -0500

    added the mobile navs animations

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
+import {PortfolioService} from '../services/portfolio.service';
 
 @Component({
   selector: 'projects',
@@ -10,13 +11,42 @@ export class ProjectsComponent implements OnInit {
   projectImagePreload = "/assets/images/octonius.png";
   bgImagePreload = "/assets/images/desk-3.png";
 
+  innerWidth: number;
+
+  paddingTop = 5;
+
   bgImageLoaded: string;
   projectImageLoaded: string;
 
-  constructor() { }
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.innerWidth = window.innerWidth;
+  }
+
+  constructor(private portfolioService: PortfolioService) { }
 
   ngOnInit() {
+    this.innerWidth = window.innerWidth;
+
+    this.portfolioService.toggleNav
+      .subscribe((res: {status}) => {
+        this.changeStyle(res.status);
+      });
   }
+
+  calculatePaddingTop() {
+    if (this.innerWidth > 900) {
+      return 15;
+    } else {
+      return this.paddingTop;
+    }
+  }
+
+  changeStyle(status) {
+    this.paddingTop = status === 'open' ? 15 : 5;
+  }
+
+
 
   determineBgImg() {
     return `linear-gradient(to right, rgba(#fff, .8), rgba(#ddd, .8)),  url(${this.bgImageLoaded})`;
