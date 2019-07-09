3a0599bba44faaa7cbb0a35a6baa3c6568206bca
Tijl Declerck <tijl.declerck@outlook.com>
2019-07-09 16:37:19 -0500

    done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -30,9 +30,10 @@ export class HomeComponent implements OnInit, OnDestroy {
    this.subscribeToNavStatus();
   }
 
-  determineBackground() {
-    return `linear-gradient(to bottom right, rgba(60,85,130,.2), rgba(9,34,79,.2)), url(${this.imgUrl})`;
-  }
+  // determineBackground() {
+  //   console.log('determining backgrend img', `linear-gradient(to bottom right, rgba(60,85,130,.2), rgba(9,34,79,.2)), url(${this.imgUrl})`);
+  //   return `linear-gradient(to bottom right, rgba(60,85,130,.2), rgba(9,34,79,.2)), url(${this.imgUrl})`;
+  // }
 
   bottomPosition() {
     return this.navStatus === 'open' && this.innerWidth < 900;
@@ -51,7 +52,7 @@ export class HomeComponent implements OnInit, OnDestroy {
   }
 
   setImgUrl() {
-      this.imgUrl = this.imgPreloadUrl;
+      this.imgUrl = `${this.imgPreloadUrl}`;
   }
 
   subscribeToNavStatus() {
