b359e17f4606362ce166bdecafd9f5016ca07cc4
Tijl Declerck <tijl.declerck@outlook.com>
2019-07-09 21:25:07 -0500

    added preload strategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {Routes, RouterModule, PreloadAllModules} from '@angular/router';
 import {HomeComponent} from './home/home.component';
 
 const routes: Routes = [
@@ -10,7 +10,12 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    // This will tell Angular to preload the lazy-loaded routes after the
+    // application has been bootstrapped. This will extend to both top-level
+    // and nested lazy-loaded modules.
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
